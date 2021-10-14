82747e6aef110442023cf6323dfe39288cc9479d
Emiel Coucke <emiel.coucke11@gmail.com>
2021-10-14 10:41:19 +0200

    oef

--- codes/line.py
+++ codes/line.py
@@ -1,8 +1,8 @@
 class line:
   def line_distance(self, x1 , y1, x2, y2):
     result = ((((x2 - x1)**2) + ((y2 - y1) ** 2)) ** 0.5)
-    return print(result)
+    return result
 
 line = line()
 
-line.line_distance(8,3,3,4)
+print(line.line_distance(8,3,3,4))
---
+++ codes/test_line.py
@@ -0,0 +1,2 @@
+def line_distance(self, x1 , y1, x2, y2):
+    return 
